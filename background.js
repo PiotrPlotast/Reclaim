a38ba6556f8215c9cfc3db6691a787135453f89c
Pietrell <piotrplotast2006@gmail.com>
2025-07-12 09:14:48 +0200

    Add web navigation listener for blocked domains
    Introduces a list of blocked domains and sets up a chrome.webNavigation.onBeforeNavigate listener to log navigation attempts to these domains.

--- background.js
+++ background.js
@@ -1,1 +1,17 @@
+const blockedDomains = [
+  "facebook.com",
+  "instagram.com",
+  "twitter.com",
+  "tiktok.com",
+  "youtube.com",
+  "reddit.com",
+];
 
+blockedDomains.forEach((blockedDomain) => {
+  chrome.webNavigation.onBeforeNavigate.addListener(
+    function (details) {
+      console.log(`onBeforeNavigate to: ${details.url}`);
+    },
+    { url: [{ hostContains: blockedDomain }] }
+  );
+});
